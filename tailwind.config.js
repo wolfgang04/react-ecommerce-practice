35addf4ed7e6b00f747582bfb4dd868f49bad6a9
Andrew <carlandrewlinao@gmail.com>
2022-11-14 20:18:56 +0800

    add screen fixed-range breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
 				"dark-grayish-blue": "hsl(219, 9%, 45%)",
 			},
 		},
+		screens: {
+			sm: { min: "320px", max: "767px" },
+			md: { min: "768px", max: "1024px" },
+			lg: { min: "1025px", max: "1440px" },
+			xl: { min: "1441px" },
+		},
 	},
 	plugins: [],
 };
