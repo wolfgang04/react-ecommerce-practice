f96cc7ff013d16dc9dddc4c0a8be9f06dd679ac4
Andrew <carlandrewlinao@gmail.com>
2022-11-20 20:36:44 +0800

    make header mobile responsive but cart in mobile layout is to be fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,10 @@ const Header = (props) => {
 	const [showCart, setShowCart] = useState(false);
 
 	const underline = "border-b-Orange border-b-4 pb-8 pt-8 text-black";
-	const menStyle = props.for === "men" || props.path === "/men" ? underline : "";
-	const womenStyle = props.for === "women" || props.path === "/women" ? underline : "";
+	const menStyle =
+		props.for === "men" || props.path === "/men" ? underline : "";
+	const womenStyle =
+		props.for === "women" || props.path === "/women" ? underline : "";
 	const pathStyle = props.pathName === "/collections" ? underline : "";
 	const aboutStyle = props.path === "/about" ? underline : "";
 	const contactStyle = props.path === "/contacts" ? underline : "";
@@ -17,16 +19,33 @@ const Header = (props) => {
 	};
 
 	return (
-		<header className="flex justify-center pb-8">
-			<div className="flex justify-between items-center w-10/12 border-b-[1px] pl-8 pr-8">
+		<header className="flex justify-center pb-8 h-[85px] sm:h-[125px]">
+			<div className="flex justify-between items-center w-5/6 border-b-[1px]">
 				{/* LEFT */}
+
 				<div className="flex flex-row justify-between w-[600px]">
-					<img
-						src={process.env.PUBLIC_URL + "../../images/logo.svg"}
-						alt="logo-img"
-						className="self-center"
-					/>
-					<span className="flex flex-row justify-between items-center w-[350px] text-dark-grayish-blue">
+					<div className="flex gap-4 lg:gap-0">
+						<button onClick={() => console.log("sdf")}>
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/images/icon-menu.svg"
+								}
+								alt=""
+								className="lg:hidden w-auto h-3 sm:h-auto"
+							/>
+						</button>
+
+						<img
+							src={
+								process.env.PUBLIC_URL + "../../images/logo.svg"
+							}
+							alt="logo-img"
+							className="self-center w-auto h-4 sm:h-5"
+						/>
+					</div>
+
+					<span className="hidden md:flex flex-row justify-between items-center w-[350px] text-dark-grayish-blue mr-4">
 						<Link to="/collections">
 							<p className={pathStyle}>Collections</p>
 						</Link>
@@ -46,7 +65,7 @@ const Header = (props) => {
 				</div>
 
 				{/* RIGHT */}
-				<div className="flex self-center w-[120px] justify-between">
+				<div className="flex w-[100px] justify-between">
 					<button
 						onClick={() => setShowCart((prevValue) => !prevValue)}
 					>
@@ -56,16 +75,17 @@ const Header = (props) => {
 								"../../images/icon-cart.svg"
 							}
 							alt=""
-							className="self-center"
+							className="self-center h-auto w-4 sm:w-6"
 						/>
 					</button>
+					
 					<img
 						src={
 							process.env.PUBLIC_URL +
 							"../../images/image-avatar.png"
 						}
 						alt=""
-						className="self-center w-12 h-auto hover:bg-Orange rounded-full"
+						className="self-center w-8 sm:w-12 h-auto hover:bg-Orange rounded-full"
 					/>
 					{showCart && <Cart onToggle={handleToggle} />}
 				</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,23 @@ module.exports = {
 			},
 		},
 		screens: {
-			sm: { min: "320px", max: "767px" },
-			md: { min: "768px", max: "1024px" },
-			lg: { min: "1025px", max: "1440px" },
-			xl: { min: "1441px" },
+			xs: "320px",
+			// => @media (min-width: 320px) { ... }
+
+			sm: "425px",
+			// => @media (min-width: 640px) { ... }
+
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
+
+			lg: "1024px",
+			// => @media (min-width: 1024px) { ... }
+
+			xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+
+			"2xl": "1536px",
+			// => @media (min-width: 1536px) { ... }
 		},
 	},
 	plugins: [],
